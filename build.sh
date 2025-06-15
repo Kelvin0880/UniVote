09229d6e501660898728884c1d13cc6942461b0d
kelvin0880 <kelvin8bp@gmail.com>
2025-06-15 14:06:29 -0400

    Fix build.sh script formatting

--- build.sh
+++ build.sh
@@ -2,13 +2,6 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
-
-python univote/manage.py collectstatic --no-input
-python univote/manage.py migrate#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
